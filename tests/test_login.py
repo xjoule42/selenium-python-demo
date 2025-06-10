66ac6d7ade691bca359888ff5fdd5f1469c5c115
Julio Soto | Software QA Engineer <90852025+xjoule42@users.noreply.github.com>
2025-06-10 22:59:38 +0000

    requirements and gitignore

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,6 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from pages.login_page import LoginPage
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carga las variables del archivo .env
 
 def test_valid_login():
     options = Options()
@@ -9,6 +13,8 @@ def test_valid_login():
     driver = webdriver.Chrome(options=options)
     login = LoginPage(driver)
     login.open()
-    login.login("tomsmith", "SuperSecretPassword!")
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+    login.login(username, password)
     assert "You logged into a secure area!" in login.get_flash_message()
     driver.quit()
